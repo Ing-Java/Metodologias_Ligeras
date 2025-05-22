bd0868137d39dd825c7dd3e8f70b1757b3ab5ab4
ingjava <ing.jalvargas@gmail.com>
2025-05-22 18:38:09 +0200

    refactor(v23): elimina dependencia de Customer con Movie usando getTitle en Rental

---
+++ carpeta21/src/main/java/usantatecla/movies/v24/Customer.java
@@ -0,0 +1,53 @@
+package usantatecla.movies.v24;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+	private String name;
+	private List<Rental> rentals;
+
+	public Customer(String name) {
+		this(name, new ArrayList<Rental>());
+	}
+
+	public Customer(String name, List<Rental> rentals) {
+		this.name = name;
+		this.rentals = rentals;
+	}
+
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String statement() {
+		String result = "Rental Record for " + this.getName() + "\n";
+		for (Rental each : rentals) {
+			result += "\t" + each.getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+		}
+		result += "Amount owed is " + String.valueOf(this.getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(this.getTotalFrequentRenterPoints()) + " frequent renter points";
+		return result;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		for (Rental each : rentals) {
+			result += each.getCharge();
+		}
+		return result;
+	}
+	
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		for (Rental each : rentals) {
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
+	}
+}
